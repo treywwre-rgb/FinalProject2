7e115b040f1cdbacf56fa0f58a682180a0c63e70
treywwre-rgb <treyw.wre@gmail.com>
2026-05-25 11:57:55 -0500

    Finished spinner

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const car = document.querySelector('.car-wrapper');
-const searchBtn = document.querySelector('button');
+document.addEventListener("DOMContentLoaded", () => {
+  feather.replace();
 
-searchBtn.addEventListener('click', () => {
-    car.classList.add('car-move');
+  const car = document.querySelector(".car-wrapper");
+  const searchBtn = document.querySelector(".searchBtn");
+
+  searchBtn.addEventListener("click", () => {
+    car.classList.add("car-move");
+    searchBtn.classList.add("loading");
+
+    const loaderSvg = feather.icons.loader.toSvg();
+    searchBtn.innerHTML = loaderSvg;
+
+    const svg = searchBtn.querySelector("svg");
+    if (svg) {
+      svg.classList.add("feather");
+    }
+  });
 });
