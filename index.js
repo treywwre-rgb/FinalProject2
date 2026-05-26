39db57049fd3a4301cc1bca862451a26bd2ee98f
treywwre-rgb <treyw.wre@gmail.com>
2026-05-26 15:14:31 -0500

    working on JS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const car = document.querySelector(".car-wrapper");
   const searchBtn = document.querySelector(".searchBtn");
+  const searchInput = document.querySelector("input");
 
   searchBtn.addEventListener("click", () => {
     car.classList.add("car-move");
@@ -15,15 +16,35 @@ document.addEventListener("DOMContentLoaded", () => {
     if (svg) {
       svg.classList.add("feather");
     }
-  const searchInput = document.querySelector("input");
+
+    searchMovies();
+  });
+
+  function showMovieDetails(imdbID) {
+    console.log(imdbID);
+  }
 
   async function searchMovies() {
-  const searchValue = searchInput.value;
-  const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=14489baa&s=${searchValue}`);
-  const data = await response.json();
+    const searchValue = searchInput.value;
+    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=14489baa&s=${searchValue}`);
+    const data = await response.json();
+    const dataListEl = document.querySelector(".data-list");
 
-  console.log(data);
-}
-  searchMovies();
-  });
+    dataListEl.innerHTML = data.Search.map((response) => responseHTML(response)).join("");
+  }
+
+  function responseHTML(response) {
+    return `<div class="movie-card" onclick="showMovieDetails (${response.imdbID})">
+      <div class="movie-card__container">
+          <h3>${response.Title}</h3>
+          <p><b>Year:</b> ${response.Year}</p>
+          <p><b>Rated:</b> ${response.Rated}</p>
+          <p><b>Released:</b> ${response.Released}</p>
+          <p><b>Runtime:</b> ${response.Runtime}</p>
+          <p><b>Genre:</b> ${response.Genre}</p>
+          <p><b>Director:</b> ${response.Director}</p>
+          <p><b>Poster:</b> <img src="${response.Poster}" alt="Movie Poster" class="movie-card__image"></p>
+      </div>
+    </div>`;    
+  }
 });
