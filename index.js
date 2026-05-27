29a18ef675b26bb0ec1707911bf58873a8e2141b
treywwre-rgb <treyw.wre@gmail.com>
2026-05-27 00:46:48 -0500

    working on JS

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=14489baa&s=${searchValue}`);
     const data = await response.json();
     const dataListEl = document.querySelector(".data-list");
-
+    if (!data.Search) {
+    dataListEl.innerHTML = "No movies found";
+    return;
+    }
     dataListEl.innerHTML = data.Search.map((response) => responseHTML(response)).join("");
   }
 
--- movie.js
+++ movie.js
@@ -1,7 +1,6 @@
 async function main() {
     const id = localStorage.getItem("imdbID");
     const movies = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=14489baa`).then((res) => res.json());
-    const movieData = await movies.json();
 
     console.log(movieData);
 }
