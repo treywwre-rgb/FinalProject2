8ab3e3efcd9bd5add6f1e835c805e53db4fcf2fa
treywwre-rgb <treyw.wre@gmail.com>
2026-05-28 12:14:40 -0500

    finishing project

--- movie.js
+++ movie.js
@@ -1,6 +1,22 @@
+const movieListEl =document.querySelector('.movie-list')
+
+
 async function main() {
     const id = localStorage.getItem("imdbID");
     const movies = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=14489baa`).then((res) => res.json());
+    const movieData = await movies.json();
+    console.log(movieData)
+
+    movieListEl.innerHTML = movieData.map(movie =>
+       ` <div class="movie">
+        <div class="movie__title">
+            Movie Title
+        </div>
+        <p class="movie__body">
+            Movie Body
+        </p>
+    </div>
+    `).join('')
 
 }
 
