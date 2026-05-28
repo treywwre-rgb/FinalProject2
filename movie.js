d62ab08582409658aee095a5dc1a5d0c31fe6227
treywwre-rgb <treyw.wre@gmail.com>
2026-05-28 13:42:14 -0500

    Working on dynamic search

--- movie.js
+++ movie.js
@@ -1,23 +1,25 @@
-const movieListEl =document.querySelector('.movie-list')
+const movieListEl = document.querySelector('.movie-list');
+const movieListEl = document.querySelector(".movies");
+const searchInput = document.querySelector(".search__input").value;
+const yearInput = document.querySelector(".year__input").value;
+const typeInput = document.querySelector(".type__input").value;
+
 
 
 async function main() {
     const id = localStorage.getItem("imdbID");
-    const movies = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=14489baa`).then((res) => res.json());
-    const movieData = await movies.json();
-    console.log(movieData)
-
-    movieListEl.innerHTML = movieData.map(movie =>
-       ` <div class="movie">
-        <div class="movie__title">
-            Movie Title
-        </div>
-        <p class="movie__body">
-            Movie Body
-        </p>
-    </div>
-    `).join('')
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=14489baa&s=$(searchInput)&y=$(yearInput)&type=$(typeInput)`   
+    ).then((res) => res.json());
 
 }
 
 main();
+
+
+movieListEl.innerHTML = movies.Search.map((movie) => {
+    return `<div class="movie">
+      <img src="${movie.Poster}" alt="${movie.Title}" />
+      <h3>${movie.Title}</h3>
+      <p>${movie.Year}</p>
+    </div>`
+}).join("")
